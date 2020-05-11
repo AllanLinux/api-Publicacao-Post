00427e2cd18a1255111652c5e56307eee1402d58
Allan Ferreira <allanlf@outlook.com.br>
2020-05-11 11:07:21 -0300

    First commit

---
+++ src/main/java/com/allan/apiPublicacaoPost/ApiPublicacaoPostApplication.java
@@ -0,0 +1,13 @@
+package com.allan.apiPublicacaoPost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPublicacaoPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPublicacaoPostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allan/apiPublicacaoPost/ApiPublicacaoPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allan.apiPublicacaoPost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPublicacaoPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
